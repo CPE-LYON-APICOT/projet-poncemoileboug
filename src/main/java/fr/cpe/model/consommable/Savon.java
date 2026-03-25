2732224050c7fb685f576e60b6e2f091a23695cd
t.poncet <t.poncet@eleve.leschartreux.net>
2026-03-25 16:10:00 +0100

    consommable done

---
+++ src/main/java/fr/cpe/model/consommable/Savon.java
@@ -0,0 +1,35 @@
+package fr.cpe.model.consommable;
+
+public class Savon implements Consommable {
+    private final String nom = "Savon";
+    private int quantite;
+    private final int seuilAlerte;
+
+    public Savon(int quantite, int seuilAlerte) {
+        this.quantite = quantite;
+        this.seuilAlerte = seuilAlerte;
+    }
+
+    @Override
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public int getQuantite() {
+        return quantite;
+    }
+
+    @Override
+    public int getSeuilAlerte() {
+        return seuilAlerte;
+    }
+
+    @Override
+    public void setQuantite(int quantite) {
+        if (quantite <= 0){
+            throw new IllegalArgumentException("La quantité de savon doit être supérieure à 0.");
+        }
+        this.quantite = quantite;
+    }
+}
