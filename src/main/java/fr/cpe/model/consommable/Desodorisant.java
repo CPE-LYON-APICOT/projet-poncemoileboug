a46cfbbbe90c636faf39ccf056c4f2413206cdf8
t.poncet <t.poncet@eleve.leschartreux.net>
2026-04-01 14:58:08 +0200

    add Desodorisant to comsomables

---
+++ src/main/java/fr/cpe/model/consommable/Desodorisant.java
@@ -0,0 +1,35 @@
+package fr.cpe.model.consommable;
+
+public class Desodorisant implements IConsommable {
+    private final String nom = "Cube desodorisant";
+    private int quantite;
+    private final int seuilAlerte;
+
+    public Desodorisant(int quantite, int seuilAlerte) {
+        this.quantite = quantite;
+        this.seuilAlerte = seuilAlerte;
+    }
+
+    @Override
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public int getQuantite() {
+        return quantite;
+    }
+
+    @Override
+    public int getSeuilAlerte() {
+        return seuilAlerte;
+    }
+
+    @Override
+    public void setQuantite(int quantite) {
+        if (quantite <= 0){
+            throw new IllegalArgumentException("La quantité de cube desodorisant doit être supérieure à 0.");
+        }
+        this.quantite = quantite;
+    }
+}
